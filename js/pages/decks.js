85cde732c3901f8b3f5b1e450a6926c9afa8c13e
Steven Gangano <stevengangano@Stevens-MacBook-Pro.local>
2016-06-29 21:32:19 -0700

    changed styling

--- js/pages/decks.js
+++ js/pages/decks.js
@@ -1,62 +1,80 @@
 
-function phone (brand, model, price, image, downPayment) { // we create an Object (team) and give it 2 parameters (mascot, qb)
+function deck (brand, model, price, image, buyLink) { // we create an Object (team) and give it 2 parameters (mascot, qb)
   this.brand = brand // “this” refers to team.
   this.model = model
   this.price = price
   this.image = image
-  this.downPayment = downPayment
-  this.phoneBalance  = function(){
-  	return this.price - this.downPayment
-  }
+  this.buyLink = buyLink
 }
 
-var phonesArray = []
+var decksArray = []
 
 // refers to the parameters above in that order (brand, model, price, image, downPayment)
-var iphone6s = new phone ('Birdhouse', 'SRV-Soccer', '43.99', 'https://cdn.ccs.com/media/catalog/product/cache/4/small_image/235x/9df78eab33525d08d6e5fb8d27136e95/p/r/primitive-amigos-bastien-skateboard-deck-8-25-bottom_1.1464279994.jpg') 
-var V10  = new phone ('Baker', 'Disco Knight', '39.99', 'https://cdn.ccs.com/media/catalog/product/cache/4/small_image/235x/9df78eab33525d08d6e5fb8d27136e95/b/a/baker-figgy-and-riley-hawk-purple-church-skateboard-deck-8-25_1.1464279803.jpg') 
-var galaxy = new phone ('Anti-Hero', 'Beres-Owls', '45.99','https://cdn.ccs.com/media/catalog/product/cache/4/small_image/235x/9df78eab33525d08d6e5fb8d27136e95/8/8/888560094550-1.1465557883.jpg') 
-var onePlus = new phone ('Blind', 'Snowstorm', '49.99', 'https://cdn.ccs.com/media/catalog/product/cache/4/small_image/235x/9df78eab33525d08d6e5fb8d27136e95/8/0/805538557763-1.1465558348.jpg')
+var primitive = new deck ('Primitive', 'Amigos Bastien', '54.95', 'https://cdn.ccs.com/media/catalog/product/cache/4/small_image/235x/9df78eab33525d08d6e5fb8d27136e95/p/r/primitive-amigos-bastien-skateboard-deck-8-25-bottom_1.1464279994.jpg', 'https://shop.ccs.com/primitive-amigos-bastien-skateboard-deck-8-25') 
+var baker  = new deck ('Baker', 'Baker Figgy and Riley Hawk Purple Church ', '54.95', 'https://cdn.ccs.com/media/catalog/product/cache/4/small_image/235x/9df78eab33525d08d6e5fb8d27136e95/b/a/baker-figgy-and-riley-hawk-purple-church-skateboard-deck-8-25_1.1464279803.jpg', 'https://shop.ccs.com/baker-figgy-and-riley-hawk-purple-church-skateboard-deck-8-25') 
+var antihero = new deck ('Anti-Hero', 'Beres Wild Unknown', '45.99','https://cdn.ccs.com/media/catalog/product/cache/4/small_image/235x/9df78eab33525d08d6e5fb8d27136e95/8/8/888560094550-1.1465557883.jpg', 'https://shop.ccs.com/antihero-beres-wild-unknown-skateboard-deck-8-38') 
+var blind = new deck ('Blind', 'SWarrior Series R7 Smith', '54.95', 'https://cdn.ccs.com/media/catalog/product/cache/4/small_image/235x/9df78eab33525d08d6e5fb8d27136e95/8/0/805538557763-1.1465558348.jpg', 'https://shop.ccs.com/blind-warrior-series-r7-smith-skateboard-deck-8-25')
 
 
 //Push each phone into the array
-phonesArray.push(iphone6s)
-phonesArray.push(V10)
-phonesArray.push(galaxy)
-phonesArray.push(onePlus)
+decksArray.push(primitive)
+decksArray.push(baker)
+decksArray.push(antihero)
+decksArray.push(blind)
+
+//sample
+decksArray.push(primitive)
+decksArray.push(baker)
+decksArray.push(antihero)
+decksArray.push(blind)
 
 
-for(i = 0; i < phonesArray.length; i++) {
+for(i = 0; i < decksArray.length; i++) {
 	//loops through each phone brand
-	var brand = document.createTextNode(phonesArray[i].brand)
+	var brand = document.createTextNode(decksArray[i].brand)
 	//loops through each phone model 
-	var model = document.createTextNode("Model: " + phonesArray[i].model)
+	var model = document.createTextNode("Model: " + decksArray[i].model)
 	//loops through each phone price
-	var price = document.createTextNode("Price: $" + phonesArray[i].price)
+	var price = document.createTextNode("Price: $" + decksArray[i].price)
 	//loops through each phone image
-	var phoneImage = phonesArray[i].image	
+	var deckImage = decksArray[i].image	
+	//link to buy a deck
+	var buy = decksArray[i].buyLink
 	//button created
-	var BuyButton = document.createElement('button')
+	
 	
 
 	var newCol= document.createElement("DIV") 
 	var newDiv = document.createElement("DIV") 
-	var brandh6 = document.createElement("H6") 
+	var brandh3 = document.createElement("H3") 
 	var modelh6 = document.createElement("H6")
 	var priceh6 = document.createElement("H6")
 	var image = document.createElement("IMG")
+	var	newAtag = document.createElement('a')
+	
+	brandh3.className = 'brandFont'
+	brandh3.appendChild(brand) 
 
-	//append brand to <h6>
-	brandh6.appendChild(brand) 
-	//append model to <h6>
+	modelh6.className = 'modelFont'
 	modelh6.appendChild(model) 
-	//append price to <h6>
+
+	priceh6.className = 'priceFont'
 	priceh6.appendChild(price) 
-	//append phone image to <img> ***need to add .src****
-	image.src = phoneImage 
-	BuyButton.innerHTML = ('Buy!')
+	//append image to <img> ***need to add .src****
+	image.src = deckImage 
+	
+
+	//Add "buy!" text to button
+	newAtag.innerHTML = ('Buy')
+	//Create a link for button
+	newAtag.setAttribute('href', buy)
+	//Opens up on a new page
+	newAtag.setAttribute('target', '_blank')
 	//Creates bootstrap button
-	BuyButton.setAttribute("class", "btn btn-danger btn-sm text-right")
+	newAtag.setAttribute("class", "btn btn-danger btn-sm text-right")
+	
+
+
 
 	//append 'col-sm-4' to <div>
 	newCol.className = "col-lg-3 col-md-4 col-sm-6 col-xs-12"
@@ -64,10 +82,10 @@ for(i = 0; i < phonesArray.length; i++) {
 	//everything is now appended to newDiv since it is the main div
 	newDiv.className = "thumbnail"
 	newDiv.appendChild(image)
-	newDiv.appendChild(brandh6)
+	newDiv.appendChild(brandh3)
 	newDiv.appendChild(modelh6) 
 	newDiv.appendChild(priceh6) 
-	newDiv.appendChild(BuyButton)
+	newDiv.appendChild(newAtag)
 
 	//newdiv is now a child of newCol. This makes each div a 4 column width.
 	newCol.appendChild(newDiv) 
--- js/pages/shirts.js
+++ js/pages/shirts.js
@@ -1,62 +1,80 @@
 
-function phone (brand, model, price, image, downPayment) { // we create an Object (team) and give it 2 parameters (mascot, qb)
+function shirt (brand, model, price, image, buyLink) { // we create an Object (team) and give it 2 parameters (mascot, qb)
   this.brand = brand // “this” refers to team.
   this.model = model
   this.price = price
   this.image = image
-  this.downPayment = downPayment
-  this.phoneBalance  = function(){
-  	return this.price - this.downPayment
-  }
+  this.buyLink = buyLink
 }
 
-var phonesArray = []
+var shirtArray = []
+
 
 // refers to the parameters above in that order (brand, model, price, image, downPayment)
-var iphone6s = new phone ('Blind', 'Curious George', '24.99', 'https://cdn.ccs.com/media/catalog/product/cache/4/small_image/235x/9df78eab33525d08d6e5fb8d27136e95/c/l/clsc-bathing-george-t-shirt-white-1.1464280744.jpg', '200.00') 
-var V10  = new phone ('Primitive', 'Jungle Fever', '24.99', 'https://cdn.ccs.com/media/catalog/product/cache/4/small_image/235x/9df78eab33525d08d6e5fb8d27136e95/p/r/primitive-top-shelf-t-shirt-black-1.1464279977.jpg', '150.00') 
-var galaxy = new phone ('Diamond Supply Co.', 'gold jewelry', '22.99','https://cdn.ccs.com/media/catalog/product/cache/4/small_image/235x/9df78eab33525d08d6e5fb8d27136e95/d/i/diamond-supply-co-jewelers-row-t-shirt-navy-1.1464279976.jpg', '100.00') 
-var onePlus = new phone ('Chocolate', 'Bunny', '25.99', 'https://cdn.ccs.com/media/catalog/product/cache/4/small_image/235x/9df78eab33525d08d6e5fb8d27136e95/c/h/chocolate-solitary-animals-premium-t-shirt-black-2.1465840253.jpg', '50.00')
+var Industrees = new shirt ('Industrees', ' Bite This ', '26.95', 'https://cdn.ccs.com/media/catalog/product/cache/4/small_image/235x/9df78eab33525d08d6e5fb8d27136e95/8/4/848434001817-1.1464279968.jpg', 'https://shop.ccs.com/casual-industrees-bite-this-t-shirt-black') 
+var Vans  = new shirt ('Vans', 'Jungle Fever', '19.95', 'https://cdn.ccs.com/media/catalog/product/cache/4/small_image/235x/9df78eab33525d08d6e5fb8d27136e95/v/a/vans-twist-up-t-shirt-black-1xx.1464279750.jpg', 'https://shop.ccs.com/vans-twist-up-t-shirt-black') 
+var Neff = new shirt ('Neff', 'Mind Control', '22.99','https://cdn.ccs.com/media/catalog/product/cache/4/small_image/235x/9df78eab33525d08d6e5fb8d27136e95/8/8/888259707860-1.1467198292.jpg', 'https://shop.ccs.com/neff-mind-control-t-shirt-white') 
+var LRG = new shirt ('LRG', 'RC Live', '25.99', 'https://cdn.ccs.com/media/catalog/product/cache/4/small_image/235x/9df78eab33525d08d6e5fb8d27136e95/l/r/lrg-rc-live-for-today-t-shirt-black-1.1464279896.jpg', 'https://shop.ccs.com/lrg-rc-live-for-today-t-shirt-black')
 
 
 //Push each phone into the array
-phonesArray.push(iphone6s)
-phonesArray.push(V10)
-phonesArray.push(galaxy)
-phonesArray.push(onePlus)
+shirtArray.push(Industrees)
+shirtArray.push(Vans)
+shirtArray.push(Neff)
+shirtArray.push(LRG)
 
+//sample
+shirtArray.push(Industrees)
+shirtArray.push(Vans)
+shirtArray.push(Neff)
+shirtArray.push(LRG)
 
-for(i = 0; i < phonesArray.length; i++) {
+for(i = 0; i < shirtArray.length; i++) {
 	//loops through each phone brand
-	var brand = document.createTextNode("Brand: " + phonesArray[i].brand)
+	var brand = document.createTextNode(shirtArray[i].brand)
 	//loops through each phone model 
-	var model = document.createTextNode("Model: " + phonesArray[i].model)
+	var model = document.createTextNode("Model: " + shirtArray[i].model)
 	//loops through each phone price
-	var price = document.createTextNode("Price: $" + phonesArray[i].price)
+	var price = document.createTextNode("Price: $" + shirtArray[i].price)
 	//loops through each phone image
-	var phoneImage = phonesArray[i].image	
+	var shirtImage = shirtArray[i].image
+	//loops through each link
+	var buy = shirtArray[i].buyLink	
 	//button created
 	var BuyButton = document.createElement('button')
 	
-
 	var newCol= document.createElement("DIV") 
 	var newDiv = document.createElement("DIV") 
-	var brandh6 = document.createElement("H6") 
+	var brandh3 = document.createElement("H3") 
 	var modelh6 = document.createElement("H6")
 	var priceh6 = document.createElement("H6")
 	var image = document.createElement("IMG")
+	var	newAtag = document.createElement('a')
+	
+	brandh3.className = 'brandFont'
+	brandh3.appendChild(brand) 
 
-	//append brand to <h6>
-	brandh6.appendChild(brand) 
-	//append model to <h6>
+	modelh6.className = 'modelFont'
 	modelh6.appendChild(model) 
-	//append price to <h6>
+
+	priceh6.className = 'priceFont'
 	priceh6.appendChild(price) 
-	//append phone image to <img> ***need to add .src****
-	image.src = phoneImage 
-	BuyButton.innerHTML = ('Buy!')
+	
+	//append shirtimage to <img> ***need to add .src****
+	image.src = shirtImage 
+	
+
+	//Add "buy!" text to button
+	newAtag.innerHTML = ('Buy')
+	//Create a link for button
+	newAtag.setAttribute('href', buy)
+	//Opens up on a new page
+	newAtag.setAttribute('target', '_blank')
 	//Creates bootstrap button
-	BuyButton.setAttribute("class", "btn btn-danger btn-sm text-right")
+	newAtag.setAttribute("class", "btn btn-danger btn-sm text-right")
+	
+
+
 
 	//append 'col-sm-4' to <div>
 	newCol.className = "col-lg-3 col-md-4 col-sm-6 col-xs-12"
@@ -64,17 +82,16 @@ for(i = 0; i < phonesArray.length; i++) {
 	//everything is now appended to newDiv since it is the main div
 	newDiv.className = "thumbnail"
 	newDiv.appendChild(image)
-	newDiv.appendChild(brandh6)
+	newDiv.appendChild(brandh3)
 	newDiv.appendChild(modelh6) 
 	newDiv.appendChild(priceh6) 
-	newDiv.appendChild(BuyButton)
+	newDiv.appendChild(newAtag)
 
 	//newdiv is now a child of newCol. This makes each div a 4 column width.
 	newCol.appendChild(newDiv) 
 
 	document.getElementById("shirts").appendChild(newCol) 
 }
-
 
 
 
