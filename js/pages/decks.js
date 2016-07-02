a044403c58125a39c374bbc54dcefac26ba60537
Steven Gangano <stevengangano@Stevens-MacBook-Pro.local>
2016-07-02 01:18:27 -0700

    added new stuff

--- js/pages/decks.js
+++ js/pages/decks.js
@@ -14,7 +14,10 @@ var primitive = new deck ('Primitive', 'Amigos Bastien', '54.95', 'https://cdn.c
 var baker  = new deck ('Baker', 'Baker Figgy and Riley Hawk Purple Church ', '54.95', 'https://cdn.ccs.com/media/catalog/product/cache/4/small_image/235x/9df78eab33525d08d6e5fb8d27136e95/b/a/baker-figgy-and-riley-hawk-purple-church-skateboard-deck-8-25_1.1464279803.jpg', 'https://shop.ccs.com/baker-figgy-and-riley-hawk-purple-church-skateboard-deck-8-25') 
 var antihero = new deck ('Anti-Hero', 'Beres Wild Unknown', '45.99','https://cdn.ccs.com/media/catalog/product/cache/4/small_image/235x/9df78eab33525d08d6e5fb8d27136e95/8/8/888560094550-1.1465557883.jpg', 'https://shop.ccs.com/antihero-beres-wild-unknown-skateboard-deck-8-38') 
 var blind = new deck ('Blind', 'SWarrior Series R7 Smith', '54.95', 'https://cdn.ccs.com/media/catalog/product/cache/4/small_image/235x/9df78eab33525d08d6e5fb8d27136e95/8/0/805538557763-1.1465558348.jpg', 'https://shop.ccs.com/blind-warrior-series-r7-smith-skateboard-deck-8-25')
-
+var creature = new deck ('Creature', 'Partanen', '49.95', 'https://cdn.ccs.com/media/catalog/product/cache/4/small_image/235x/9df78eab33525d08d6e5fb8d27136e95/c/r/creature-partanen-shakra-skateboard-deck-8-2-1_1.1465300335.jpg','https://shop.ccs.com/creature-partanen-shakra-skateboard-deck-8-20')
+var habitat = new deck ('Habitat', 'Dot Gain Green', '49.95', 'https://cdn.ccs.com/media/catalog/product/cache/4/small_image/235x/9df78eab33525d08d6e5fb8d27136e95/h/a/habitat-dot-gain-green-skateboard-deck-8-375-bottom_1.1464279666.jpg', 'https://shop.ccs.com/habitat-dot-gain-green-skateboard-deck-8-375')
+var mystery = new deck ('Mystery', 'Cosmic Heart V2', '54.95', 'https://cdn.ccs.com/media/catalog/product/cache/4/small_image/235x/9df78eab33525d08d6e5fb8d27136e95/m/y/mystery-cosmic-heart-v2-skateboard-deck-8-125-1_1.1465558035.jpg', 'https://shop.ccs.com/mystery-cosmic-heart-v2-skateboard-deck-8-125')
+var chocolate = new deck ('Chocolate', 'Raven Tershy Trunk Boys', '46.76', 'https://cdn.ccs.com/media/catalog/product/cache/4/small_image/235x/9df78eab33525d08d6e5fb8d27136e95/c/h/chocolate-raven-tershy-trunk-boyz-skateboard-deck-8-375-bottom_1.1464279769.jpg', 'https://shop.ccs.com/chocolate-raven-tershy-trunk-boyz-skateboard-deck-8-375')
 
 //Push each phone into the array
 decksArray.push(primitive)
@@ -23,9 +26,9 @@ decksArray.push(antihero)
 decksArray.push(blind)
 
 //sample
-decksArray.push(primitive)
-decksArray.push(baker)
-decksArray.push(antihero)
+decksArray.push(creature)
+decksArray.push(habitat)
+decksArray.push(mystery)
 decksArray.push(blind)
 
 
@@ -42,7 +45,6 @@ for(i = 0; i < decksArray.length; i++) {
 	var buy = decksArray[i].buyLink
 	//button created
 	
-	
 
 	var newCol= document.createElement("DIV") 
 	var newDiv = document.createElement("DIV") 
@@ -52,6 +54,8 @@ for(i = 0; i < decksArray.length; i++) {
 	var image = document.createElement("IMG")
 	var	newAtag = document.createElement('a')
 	
+	
+
 	brandh3.className = 'brandFont'
 	brandh3.appendChild(brand) 
 
@@ -72,6 +76,7 @@ for(i = 0; i < decksArray.length; i++) {
 	newAtag.setAttribute('target', '_blank')
 	//Creates bootstrap button
 	newAtag.setAttribute("class", "btn btn-danger btn-sm text-right")
+
 	
 
 
@@ -80,7 +85,7 @@ for(i = 0; i < decksArray.length; i++) {
 	newCol.className = "col-lg-3 col-md-4 col-sm-6 col-xs-12"
 	
 	//everything is now appended to newDiv since it is the main div
-	newDiv.className = "thumbnail"
+	newDiv.className = "background thumbnail text-center"
 	newDiv.appendChild(image)
 	newDiv.appendChild(brandh3)
 	newDiv.appendChild(modelh6) 
--- js/pages/shirts.js
+++ js/pages/shirts.js
@@ -15,19 +15,21 @@ var Industrees = new shirt ('Industrees', ' Bite This ', '26.95', 'https://cdn.c
 var Vans  = new shirt ('Vans', 'Jungle Fever', '19.95', 'https://cdn.ccs.com/media/catalog/product/cache/4/small_image/235x/9df78eab33525d08d6e5fb8d27136e95/v/a/vans-twist-up-t-shirt-black-1xx.1464279750.jpg', 'https://shop.ccs.com/vans-twist-up-t-shirt-black') 
 var Neff = new shirt ('Neff', 'Mind Control', '22.99','https://cdn.ccs.com/media/catalog/product/cache/4/small_image/235x/9df78eab33525d08d6e5fb8d27136e95/8/8/888259707860-1.1467198292.jpg', 'https://shop.ccs.com/neff-mind-control-t-shirt-white') 
 var LRG = new shirt ('LRG', 'RC Live', '25.99', 'https://cdn.ccs.com/media/catalog/product/cache/4/small_image/235x/9df78eab33525d08d6e5fb8d27136e95/l/r/lrg-rc-live-for-today-t-shirt-black-1.1464279896.jpg', 'https://shop.ccs.com/lrg-rc-live-for-today-t-shirt-black')
-
-
+var hundreds = new shirt ('The Hundreds', 'Forever Bar', '24.95', 'https://cdn.ccs.com/media/catalog/product/cache/4/small_image/235x/9df78eab33525d08d6e5fb8d27136e95/t/h/the-hundreds-forever-bar-t-shirt-charcoal-heather-1.1464279904.jpg', 'https://shop.ccs.com/the-hundreds-forever-bar-t-shirt-charcoal-heather' )
+var welcome = new shirt ('Welcome', 'Miller Faces', '22.95', 'https://cdn.ccs.com/media/catalog/product/cache/4/small_image/235x/9df78eab33525d08d6e5fb8d27136e95/w/e/welcome-miller-faces-t-shirt-teal-2.1464279896.jpg', 'https://shop.ccs.com/welcome-miller-faces-t-shirt-teal')
+var enjoi = new shirt ('Enjoi', 'No Brainer', '19.60', 'https://cdn.ccs.com/media/catalog/product/cache/4/small_image/235x/9df78eab33525d08d6e5fb8d27136e95/e/n/enjoi-no-brainer-t-shirt-white-1.1464279896.jpg', 'https://shop.ccs.com/enjoi-no-brainer-t-shirt-white')
+var santaCruz = new shirt ('Santa Cruz', 'Warp Dot', '19.95', 'https://cdn.ccs.com/media/catalog/product/cache/4/small_image/235x/9df78eab33525d08d6e5fb8d27136e95/s/a/santa-cruz-warp-dot-t-shirt-black-1.1464279904.jpg', 'https://shop.ccs.com/santa-cruz-warp-dot-t-shirt-black')
 //Push each phone into the array
 shirtArray.push(Industrees)
 shirtArray.push(Vans)
 shirtArray.push(Neff)
 shirtArray.push(LRG)
 
-//sample
-shirtArray.push(Industrees)
-shirtArray.push(Vans)
-shirtArray.push(Neff)
-shirtArray.push(LRG)
+
+shirtArray.push(hundreds)
+shirtArray.push(welcome)
+shirtArray.push(enjoi)
+shirtArray.push(santaCruz)
 
 for(i = 0; i < shirtArray.length; i++) {
 	//loops through each phone brand
@@ -80,7 +82,7 @@ for(i = 0; i < shirtArray.length; i++) {
 	newCol.className = "col-lg-3 col-md-4 col-sm-6 col-xs-12"
 	
 	//everything is now appended to newDiv since it is the main div
-	newDiv.className = "thumbnail"
+	newDiv.className = "background thumbnail text-center"
 	newDiv.appendChild(image)
 	newDiv.appendChild(brandh3)
 	newDiv.appendChild(modelh6) 
