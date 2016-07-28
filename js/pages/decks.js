24529998278941d8eac14db6051eeec2b8db072b
Steven Gangano <stevengangano@Stevens-MacBook-Pro.local>
2016-07-28 12:27:19 -0700

    cleaned up code

--- js/pages/decks.js
+++ js/pages/decks.js
@@ -1,6 +1,6 @@
 
-function deck (brand, model, price, image, buyLink) { // we create an Object (team) and give it 2 parameters (mascot, qb)
-  this.brand = brand // “this” refers to team.
+function deck (brand, model, price, image, buyLink) { 
+  this.brand = brand 
   this.model = model
   this.price = price
   this.image = image
@@ -9,7 +9,6 @@ function deck (brand, model, price, image, buyLink) { // we create an Object (te
 
 var decksArray = []
 
-// refers to the parameters above in that order (brand, model, price, image, downPayment)
 var primitive = new deck ('Primitive', 'Amigos Bastien', '54.95', 'https://cdn.ccs.com/media/catalog/product/cache/4/small_image/235x/9df78eab33525d08d6e5fb8d27136e95/p/r/primitive-amigos-bastien-skateboard-deck-8-25-bottom_1.1464279994.jpg', 'https://shop.ccs.com/primitive-amigos-bastien-skateboard-deck-8-25') 
 var baker  = new deck ('Baker', 'Baker Figgy and Riley Hawk Purple Church ', '54.95', 'https://cdn.ccs.com/media/catalog/product/cache/4/small_image/235x/9df78eab33525d08d6e5fb8d27136e95/b/a/baker-figgy-and-riley-hawk-purple-church-skateboard-deck-8-25_1.1464279803.jpg', 'https://shop.ccs.com/baker-figgy-and-riley-hawk-purple-church-skateboard-deck-8-25') 
 var antihero = new deck ('Anti-Hero', 'Beres Wild Unknown', '45.99','https://cdn.ccs.com/media/catalog/product/cache/4/small_image/235x/9df78eab33525d08d6e5fb8d27136e95/8/8/888560094550-1.1465557883.jpg', 'https://shop.ccs.com/antihero-beres-wild-unknown-skateboard-deck-8-38') 
@@ -19,13 +18,11 @@ var habitat = new deck ('Habitat', 'Dot Gain Green', '49.95', 'https://cdn.ccs.c
 var mystery = new deck ('Mystery', 'Cosmic Heart V2', '54.95', 'https://cdn.ccs.com/media/catalog/product/cache/4/small_image/235x/9df78eab33525d08d6e5fb8d27136e95/m/y/mystery-cosmic-heart-v2-skateboard-deck-8-125-1_1.1465558035.jpg', 'https://shop.ccs.com/mystery-cosmic-heart-v2-skateboard-deck-8-125')
 var chocolate = new deck ('Chocolate', 'Raven Tershy Trunk Boys', '46.76', 'https://cdn.ccs.com/media/catalog/product/cache/4/small_image/235x/9df78eab33525d08d6e5fb8d27136e95/c/h/chocolate-raven-tershy-trunk-boyz-skateboard-deck-8-375-bottom_1.1464279769.jpg', 'https://shop.ccs.com/chocolate-raven-tershy-trunk-boyz-skateboard-deck-8-375')
 
-//Push each phone into the array
+//Push each deck into the array
 decksArray.push(primitive)
 decksArray.push(baker)
 decksArray.push(antihero)
 decksArray.push(blind)
-
-//sample
 decksArray.push(creature)
 decksArray.push(habitat)
 decksArray.push(mystery)
@@ -33,19 +30,18 @@ decksArray.push(blind)
 
 
 for(i = 0; i < decksArray.length; i++) {
-	//loops through each phone brand
+	//loops through each deck brand
 	var brand = document.createTextNode(decksArray[i].brand)
-	//loops through each phone model 
+	//loops through each deck model 
 	var model = document.createTextNode("Model: " + decksArray[i].model)
-	//loops through each phone price
+	//loops through each deck price
 	var price = document.createTextNode("Price: $" + decksArray[i].price)
-	//loops through each phone image
+	//loops through each deck image
 	var deckImage = decksArray[i].image	
 	//link to buy a deck
 	var buy = decksArray[i].buyLink
 	//button created
 	
-
 	var newCol= document.createElement("DIV") 
 	var newDiv = document.createElement("DIV") 
 	var brandh3 = document.createElement("H3") 
@@ -54,8 +50,6 @@ for(i = 0; i < decksArray.length; i++) {
 	var image = document.createElement("IMG")
 	var	newAtag = document.createElement('a')
 	
-	
-
 	brandh3.className = 'brandFont'
 	brandh3.appendChild(brand) 
 
@@ -67,7 +61,6 @@ for(i = 0; i < decksArray.length; i++) {
 	//append image to <img> ***need to add .src****
 	image.src = deckImage 
 	
-
 	//Add "buy!" text to button
 	newAtag.innerHTML = ('Buy')
 	//Create a link for button
@@ -76,10 +69,6 @@ for(i = 0; i < decksArray.length; i++) {
 	newAtag.setAttribute('target', '_blank')
 	//Creates bootstrap button
 	newAtag.setAttribute("class", "btn btn-danger btn-sm text-right")
-
-	
-
-
 
 	//append 'col-sm-4' to <div>
 	newCol.className = "col-lg-3 col-md-4 col-sm-6 col-xs-12"
--- js/pages/shirts.js
+++ js/pages/shirts.js
@@ -1,6 +1,6 @@
 
-function shirt (brand, model, price, image, buyLink) { // we create an Object (team) and give it 2 parameters (mascot, qb)
-  this.brand = brand // “this” refers to team.
+function shirt (brand, model, price, image, buyLink) { 
+  this.brand = brand 
   this.model = model
   this.price = price
   this.image = image
@@ -9,38 +9,35 @@ function shirt (brand, model, price, image, buyLink) { // we create an Object (t
 
 var shirtArray = []
 
-
-// refers to the parameters above in that order (brand, model, price, image, downPayment)
 var Industrees = new shirt ('Industrees', ' Bite This ', '26.95', 'https://cdn.ccs.com/media/catalog/product/cache/4/small_image/235x/9df78eab33525d08d6e5fb8d27136e95/8/4/848434001817-1.1464279968.jpg', 'https://shop.ccs.com/casual-industrees-bite-this-t-shirt-black') 
 var Vans  = new shirt ('Vans', 'Jungle Fever', '19.95', 'https://cdn.ccs.com/media/catalog/product/cache/4/small_image/235x/9df78eab33525d08d6e5fb8d27136e95/v/a/vans-twist-up-t-shirt-black-1xx.1464279750.jpg', 'https://shop.ccs.com/vans-twist-up-t-shirt-black') 
 var Neff = new shirt ('Neff', 'Mind Control', '22.99','https://cdn.ccs.com/media/catalog/product/cache/4/small_image/235x/9df78eab33525d08d6e5fb8d27136e95/8/8/888259707860-1.1467198292.jpg', 'https://shop.ccs.com/neff-mind-control-t-shirt-white') 
 var LRG = new shirt ('LRG', 'RC Live', '25.99', 'https://cdn.ccs.com/media/catalog/product/cache/4/small_image/235x/9df78eab33525d08d6e5fb8d27136e95/l/r/lrg-rc-live-for-today-t-shirt-black-1.1464279896.jpg', 'https://shop.ccs.com/lrg-rc-live-for-today-t-shirt-black')
 var hundreds = new shirt ('The Hundreds', 'Forever Bar', '24.95', 'https://cdn.ccs.com/media/catalog/product/cache/4/small_image/235x/9df78eab33525d08d6e5fb8d27136e95/t/h/the-hundreds-forever-bar-t-shirt-charcoal-heather-1.1464279904.jpg', 'https://shop.ccs.com/the-hundreds-forever-bar-t-shirt-charcoal-heather' )
-var welcome = new shirt ('Welcome', 'Miller Faces', '22.95', 'https://cdn.ccs.com/media/catalog/product/cache/4/small_image/235x/9df78eab33525d08d6e5fb8d27136e95/w/e/welcome-miller-faces-t-shirt-teal-2.1464279896.jpg', 'https://shop.ccs.com/welcome-miller-faces-t-shirt-teal')
+var welcome = new shirt ('Welcome', 'Miller Faces', '22.95', 'img/miller.jpg')
 var enjoi = new shirt ('Enjoi', 'No Brainer', '19.60', 'https://cdn.ccs.com/media/catalog/product/cache/4/small_image/235x/9df78eab33525d08d6e5fb8d27136e95/e/n/enjoi-no-brainer-t-shirt-white-1.1464279896.jpg', 'https://shop.ccs.com/enjoi-no-brainer-t-shirt-white')
 var santaCruz = new shirt ('Santa Cruz', 'Warp Dot', '19.95', 'https://cdn.ccs.com/media/catalog/product/cache/4/small_image/235x/9df78eab33525d08d6e5fb8d27136e95/s/a/santa-cruz-warp-dot-t-shirt-black-1.1464279904.jpg', 'https://shop.ccs.com/santa-cruz-warp-dot-t-shirt-black')
-//Push each phone into the array
+
+//Push each shirt into the array
 shirtArray.push(Industrees)
 shirtArray.push(Vans)
 shirtArray.push(Neff)
 shirtArray.push(LRG)
-
-
 shirtArray.push(hundreds)
 shirtArray.push(welcome)
 shirtArray.push(enjoi)
 shirtArray.push(santaCruz)
 
 for(i = 0; i < shirtArray.length; i++) {
-	//loops through each phone brand
+	//loops through each shirt brand
 	var brand = document.createTextNode(shirtArray[i].brand)
-	//loops through each phone model 
+	//loops through each shirt model 
 	var model = document.createTextNode("Model: " + shirtArray[i].model)
-	//loops through each phone price
+	//loops through each shirt price
 	var price = document.createTextNode("Price: $" + shirtArray[i].price)
-	//loops through each phone image
+	//loops through each shirt image
 	var shirtImage = shirtArray[i].image
-	//loops through each link
+	//loops through shirt link
 	var buy = shirtArray[i].buyLink	
 	//button created
 	var BuyButton = document.createElement('button')
@@ -75,9 +72,6 @@ for(i = 0; i < shirtArray.length; i++) {
 	//Creates bootstrap button
 	newAtag.setAttribute("class", "btn btn-danger btn-sm text-right")
 	
-
-
-
 	//append 'col-sm-4' to <div>
 	newCol.className = "col-lg-3 col-md-4 col-sm-6 col-xs-12"
 	
